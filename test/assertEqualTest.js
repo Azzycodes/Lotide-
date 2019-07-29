5c0a7d93b46f1756afe054078769fb8f39361a23
cd <azzamathar@hotmail.com>
2019-07-29 16:53:05 +0000

    created seperate assertEqualTest to test

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("We did it!", "We did it!");
+assertEqual(1, 1);
+assertEqual(1,3);
