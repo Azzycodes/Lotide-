d6beff4008fcbc78956e2d05ed628caee84ebb42
cd <azzamathar@hotmail.com>
2019-07-29 20:52:23 +0000

    removed our test code with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
 
 
 
-// TEST CODE
-assertEqual(head([25,1,764]), 25);
-assertEqual(head(["Came", "Saw", "conquered"]), "Saw");
+// // TEST CODE
+// assertEqual(head([25,1,764]), 25);
+// assertEqual(head(["Came", "Saw", "conquered"]), "Saw");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);  
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
