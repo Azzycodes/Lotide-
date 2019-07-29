e96186abd93ca8ea851b42301b114d230c0760d5
cd <azzamathar@hotmail.com>
2019-07-29 21:10:32 +0000

    using mocha and chai to test functions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail = require('../tail');
+
+
+// // TEST CODE
+// const words = ["Yo Yo", "Lighthouse", "Labs"]; 
+// assertArraysEqual(tail((words)), ["Lighthouse", "Labs"]);  
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"]; 
-assertArraysEqual(tail((words)), ["Lighthouse", "Labs"]);  
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);  
+    });
+  });
