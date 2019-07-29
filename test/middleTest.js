f0daea0f7e6887ff2990f3ae730ea054a1f32650
cd <azzamathar@hotmail.com>
2019-07-29 21:10:17 +0000

    using mocha and chai to test functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-const eqArrays = require('../eqArrays');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+// const eqArrays = require('../eqArrays');
 
-// Test Code
-eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), []), false) // Length of Arrays not equal
-eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true) // => [3, 4]
+// // Test Code
+// eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), []), false) // Length of Arrays not equal
+// eqArrays(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true) // => [3, 4]
+
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
