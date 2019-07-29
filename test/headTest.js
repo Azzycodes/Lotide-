ce4deaa88d50dfe25793747fcc12c213d1542722
cd <azzamathar@hotmail.com>
2019-07-29 16:52:37 +0000

    created seperate file to test code

---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+
+
+// TEST CODE
+assertEqual(head([25,1,764]), 25);
+assertEqual(head(["Came", "Saw", "conquered"]), "Saw");
