d9761e76a72f36b7dfd850e681c33a3efccd796e
cd <azzamathar@hotmail.com>
2019-07-29 17:29:44 +0000

    fixed assertion to pass on expected value

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,7 @@
-const assertEqual = require('../assertEqual');
+const assertArraysEqual = require('../assertArraysEqual');
 const tail = require('../tail');
 
 
 // TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const words = ["Yo Yo", "Lighthouse", "Labs"]; 
+assertArraysEqual(tail((words)), ["Lighthouse", "Labs"]);  
